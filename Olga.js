443f71c06cf119c51cb648630155f657f894a91e
ilina19.08@yandex.ru <ilina19.08@yandex.ru>
2021-03-25 21:56:06 +0500

    форматирование кода

--- Olga.js
+++ Olga.js
@@ -1,11 +1,12 @@
 var i=0;
-  for(var i=0; i<=999; i++){
-    if(i<10){
+  for(var i=0; i<=999; i++) {
+    if(i<10) {
       console.log("00"+i);
     }
-    else if(i<100){
+    else if(i<100) {
       console.log("0"+i);
     }
-    else 
+    else {
         console.log(i);
+    }
   }

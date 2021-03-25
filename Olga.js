d90c89a478fae7c56a40c27c3f7923219d6b66e7
ilina19.08@yandex.ru <ilina19.08@yandex.ru>
2021-03-25 21:52:42 +0500

    программа с задачейц про числа с 0

---
+++ Olga.js
@@ -0,0 +1,11 @@
+var i=0;
+  for(var i=0; i<=999; i++){
+    if(i<10){
+      console.log("00"+i);
+    }
+    else if(i<100){
+      console.log("0"+i);
+    }
+    else 
+        console.log(i);
+  }
